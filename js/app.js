8b8f6ca2ed80c860f493ff55934bdc021f8508f6
BellaBradbury <bellabradbury@yahoo.com>
2025-12-21 09:53:25 -0500

    mb nav menu btn styles updated

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 /* ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- -------- GLOBAL VARS */
-let navToggle = document.getElementById('nav-menu').firstChild;
-let navBtns = Array.from( document.getElementsByClassName('nav-btn') );
+let navToggle = document.getElementById('nav-menu');
+let navMenuImg = document.getElementById('nav-menu').firstChild;
+let navBtns = document.getElementById('navigation');
 
 // /* ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- -------- MOBILE MENU */
-function menuSwitch(status, arrow, vis=null) {
+function menuSwitch(status, img, vis=null) {
     navToggle.setAttribute('data-flag', status);
-    navBtns.forEach(btn => { 
-        if (vis) { btn.style.display = 'block' } else { btn.style.display = 'none' }
-    });
-    navToggle.innerHTML = arrow;
+    if (vis) { navBtns.style.display = 'flex' } else { navBtns.style.display = 'none' }
+    navMenuImg.src = img;
 }
 
 navToggle.addEventListener('click', (e)=> {
-    let status = e.target.getAttribute('data-flag');
-    if (status === 'up') { menuSwitch('down', '&#9650;', 'vis') } else { menuSwitch('up', '&#9660;') }
+    let status = navToggle.getAttribute('data-flag');
+    if (status === 'up') { menuSwitch('down', 'images/menu-btns/nav-menu-v.png', 'vis') } else { menuSwitch('up', 'images/menu-btns/nav-menu-h.png') }
+});
+navMenuImg.addEventListener('click', (e)=> {
+    e.stopPropagation();
+    let status = navToggle.getAttribute('data-flag');
+    if (status === 'up') { menuSwitch('down', 'images/menu-btns/nav-menu-v.png', 'vis') } else { menuSwitch('up', 'images/menu-btns/nav-menu-h.png') }
 });
 
 /* ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- --------- ACTIVE NAV */

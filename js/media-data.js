6ad6a4b9994f5c732614fc3324698f985697b707
BellaBradbury <bellabradbury@yahoo.com>
2025-12-26 15:35:24 -0500

    media page coded

---
+++ js/media-data.js
@@ -0,0 +1,18 @@
+// TEMPLATE:
+
+// {
+//     mediatitle: "",
+//     fav: "YES/NO",
+//     eventdate: 00/00/0000,
+//     eventtitle: "",
+//     mediatype: "VIDEO/PHOTO/POSTER",
+//     picture: {
+//       medium: "",
+//       thumbnail: "",
+//     },
+//     description: ""
+// }
+
+const data = [
+
+]

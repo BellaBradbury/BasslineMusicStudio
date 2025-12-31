be89f7d0589fce8f7872f16446b56faf5894748a
BellaBradbury <bellabradbury@yahoo.com>
2025-12-31 02:25:38 -0500

    read more button styling fixed

--- js/team.js
+++ js/team.js
@@ -1,3 +1,7 @@
+// Screen Size Variables
+let wTablet = window.matchMedia("(min-width: 768px)");
+let wDesktop = window.matchMedia("(min-width: 1024px)");
+
 // Bella Bradbury Variables
 let bbBtn = document.getElementById('bb-more-btn');
 let bbMore = document.getElementById('bb-more-sect');
@@ -16,23 +20,43 @@ let ekMore = document.getElementById('ek-more-sect');
 let ekHide = document.getElementById('ek-hide-btn');
 let ekArr = [ekBtn, ekMore, ekHide]
 
+let allArr = [bbArr, zbArr, ekArr]
+
 // function to show or hide more bio text depending on button click
-function showMore(btn, memberArr) {
+function showMore(btn, mId) {
     if (btn.textContent.includes('more')) {
-        memberArr[0].style.display = 'none';
-        memberArr[1].style.display = 'block';
+        for (let i = 0; i < allArr.length; i ++) {
+            if (i === mId) {
+                allArr[i][0].style.display = 'none';
+                allArr[i][1].style.display = 'block';
+            } else {
+                allArr[i][1].style.display = 'none';
+                allArr[i][0].style.display = 'block';
+            }
+        }
     } else {
-        memberArr[1].style.display = 'none';
-        memberArr[0].style.display = 'block';
+        allArr[mId][1].style.display = 'none';
+        allArr[mId][0].style.display = 'block';
     }
 }
 
-// Bella Bradbury Event Listeners
-bbBtn.addEventListener('click',(e)=>{showMore(e.target, bbArr)});
-bbHide.addEventListener('click',(e)=>{showMore(e.target, bbArr)});
-// Zack Bradbury Event Listeners
-zbBtn.addEventListener('click',(e)=>{showMore(e.target, zbArr)});
-zbHide.addEventListener('click',(e)=>{showMore(e.target, zbArr)});
-// Evelyn Koop Event Listeners
-ekBtn.addEventListener('click',(e)=>{showMore(e.target, ekArr)});
-ekHide.addEventListener('click',(e)=>{showMore(e.target, ekArr)});
+function handleChange(btn) {
+    let staffMember = btn.id.slice(0, 2)
+    var mId;
+    
+    if (staffMember === 'bb') {mId = 0} 
+    else if (staffMember === 'zb') {mId = 1} 
+    else if (staffMember === 'ek') {mId = 2}
+    
+    showMore(btn, mId)
+}
+
+    // Bella Bradbury Event Listeners
+    bbBtn.addEventListener('click',(e)=>{handleChange(e.target)});
+    bbHide.addEventListener('click',(e)=>{handleChange(e.target)});
+    // Zack Bradbury Event Listeners
+    zbBtn.addEventListener('click',(e)=>{handleChange(e.target)});
+    zbHide.addEventListener('click',(e)=>{handleChange(e.target)});
+    // Evelyn Koop Event Listeners
+    ekBtn.addEventListener('click',(e)=>{handleChange(e.target)});
+    ekHide.addEventListener('click',(e)=>{handleChange(e.target)});
